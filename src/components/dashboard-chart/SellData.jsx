763279e1bba3ebdedc49b6f77e4161dafa182fbb
firmnardians <firmnardians@gmail.com>
2020-02-24 02:54:07 +0700

    Masih dalam proses pengembangan, karena ini dibuat tanpa framework css, jadi butuh waktu yang cukup lama untuk design nya

--- src/components/dashboard-chart/SellData.jsx
+++ src/components/dashboard-chart/SellData.jsx
@@ -41,7 +41,7 @@ const data = {
       pointHoverBorderWidth: 2,
       pointRadius: 1,
       pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40, 45]
+      data: [65, 59, 80, 81, 56, 55, 40, 45, 51]
     }
   ]
 };
